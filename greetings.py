29764d09e4c14ddbaccb8d91d4028a9d2d9f0bdb
Bart Feenstra <bart@mynameisbart.com>
2018-04-22 15:08:41 +0200

    Greetings, earthlings! This is the initial commit.

---
+++ greetings.py
@@ -0,0 +1,14 @@
+from __future__ import print_function
+
+import random
+
+GREETINGS = [
+    'Hi, y\'all',
+    'G\'day, mate!',
+]
+
+def greeting():
+    return random.choice(GREETINGS)
+
+if __name__ == '__main__':
+    print(greeting())
